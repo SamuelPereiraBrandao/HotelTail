32a37abcb877ff8d66f3605583321307f7502260
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-09-30 16:43:59 -0300

    add

--- src/components/ReservaComp/ReservaInicial.vue
+++ src/components/ReservaComp/ReservaInicial.vue
@@ -10,7 +10,7 @@
             <div class="">
                
                     <router-link to="/informacoes"
-                        class="hover:text-white transition-all inline-flex items-center text-white bg-black hover:bg-white hover:text-black rounded font-medium px-3 py-2">
+                        class=" transition-all inline-flex items-center text-white bg-black hover:bg-white hover:text-black rounded font-medium px-3 py-2">
 
                        
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
